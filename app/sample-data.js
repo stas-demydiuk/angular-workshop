e06a88f3097fec76867cc403c468ddd3e9f4a2c6
Stanislav Demydiuk <s.demydiuk@gmail.com>
2016-11-09 12:45:43 +0200

    [Step4] Add mock data

---
+++ app/sample-data.js
@@ -0,0 +1,8 @@
+angular
+    .module('app')
+    .value('tasks', [
+        {name: 'Task1', description: 'TaskDescription1'},
+        {name: 'Task2', description: 'TaskDescription2'},
+        {name: 'Task3', description: 'TaskDescription3'},
+        {name: 'Task4', description: 'TaskDescription4'}
+    ]);
