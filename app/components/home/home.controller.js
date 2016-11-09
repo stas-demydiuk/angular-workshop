75abd5e2f1647193bc02977852e887d5204602ae
Stanislav Demydiuk <s.demydiuk@gmail.com>
2016-11-09 12:51:22 +0200

    [Step4-2] Show task list on home page

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,5 +1,5 @@
-angular.module('app').controller('HomeController', function () {
+angular.module('app').controller('HomeController', function (tasks) {
     var vm = this;
 
-    vm.message = 'Hello Angular';
+    vm.tasks = tasks;
 });
